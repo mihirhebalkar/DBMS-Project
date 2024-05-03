d0a50716155296962dcef7af0803d44c7f91aaec
Mihir Hebalkar <mihirhebalkar07@gmail.com>
2024-05-03 16:31:10 +0530

    Update Ollivanders.sql

--- Ollivanders.sql
+++ Ollivanders.sql
@@ -1,10 +1,12 @@
-select wid, wp.age, w.coins needed, w.power
 
-from wands w join wands property wp on w.code = wp.code 
-
-where wp.is_evil=0 and w.coins_needed =
-(
-SELECT min(w2.coins_needed) 
-from wands w2 join wands property wp2 on w2.code = wp2.code 
-where wp2.age=wp.age and w2.power=w.power and wp2.is_evil = 0) 
-order by w.power desc, wp.age desc;
+select w.id,  wp.age, w.coins_needed, w.power 
+from wands w 
+  join wands_property wp on w.code = wp.code 
+where wp.is_evil=0 and w.coins_needed = 
+  (
+  SELECT min(w2.coins_needed) 
+  from wands w2 
+    join wands_property wp2 on w2.code = wp2.code 
+  where wp2.age=wp.age and w2.power=w.power and wp2.is_evil = 0
+  ) 
+  order by w.power desc, wp.age desc;
